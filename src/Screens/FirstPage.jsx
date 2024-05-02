4a11ee6c5772660f0bea0a1e6b018320d4d6beca
YahaniNethmini <tynperera@gmail.com>
2024-05-02 23:29:26 +0530

    Changing the firstpage

--- src/Screens/FirstPage.jsx
+++ src/Screens/FirstPage.jsx
@@ -4,10 +4,26 @@ import Logo from '../assets/RecipeRoundup.png'
 
 function FirstPage(){
     return(
-        <div className="background">
+        <div>
             <div className="logo-container">
                 <img src={Logo} alt="logo" className="logo" />
             </div>
+            <div className="background">
+                <div class="text-animation-container">
+                    <h1>... Welcome to RecipeRoundup ...</h1>
+                </div>
+                <div className="log">
+                <h3>
+                    Already Have an Account, LogIn : <br />
+                    <button className="button1">LogIn</button>
+                </h3>
+                <h3>
+                    Doesn't Have an Account, SignUp : <br />
+                    <button className="button2">SignUp</button>
+                </h3>
+                </div>
+            </div>
+
         </div>
     )
 }
